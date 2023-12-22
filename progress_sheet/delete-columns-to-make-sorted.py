4dd25618258c560a232f65f936429aa5ee9fd847
Mikias Berhanu <122903816+Myk72@users.noreply.github.com>
2023-12-22 13:03:42 +0300

    Added solution for Delete Columns to Make Sorted

---
+++ progress_sheet/delete-columns-to-make-sorted.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minDeletionSize(self, strs: List[str]) -> int:
+        count=0
+        for j in range(len(strs[0])):
+            for i in range(1,len(strs)):
+                if strs[i-1][j]>strs[i][j]:
+                    count+=1
+                    break
+        return count
+                
